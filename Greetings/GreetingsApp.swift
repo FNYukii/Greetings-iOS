e8d60cf07ae7c2a0d0defa06a082db9ffe44ca11
Yu357 <forestgreen14689@gmail.com>
2022-05-06 12:57:48 +0900

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Yu on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
