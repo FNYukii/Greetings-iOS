17332dab0d8b89b7bb159e1baf6413fe8029258f
Yu357 <forestgreen14689@gmail.com>
2022-05-06 20:52:13 +0900

    feat: FireUserクラスを作成

---
+++ Greetings/utilities/FireUser.swift
@@ -0,0 +1,26 @@
+//
+//  FireUser.swift
+//  Greetings
+//
+//  Created by Yu on 2022/05/06.
+//
+
+class FireUser {
+    
+    static func read(id: String) {
+        
+    }
+    
+    static func create() {
+        
+    }
+    
+    static func update() {
+        
+    }
+    
+    static func delete() {
+        
+    }
+    
+}
