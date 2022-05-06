c66ff6fe79eccad3e2a7f11a38c19d3bda9f7f4c
Yu357 <forestgreen14689@gmail.com>
2022-05-06 20:30:21 +0900

    feat: postを取り扱うためのクラスを作成

---
+++ Greetings/utilities/FirePost.swift
@@ -0,0 +1,17 @@
+//
+//  FirePost.swift
+//  Greetings
+//
+//  Created by Yu on 2022/05/06.
+//
+
+class FirePost {
+    
+    static func create(text: String) {
+        // TODO: Add new post
+    }
+    
+    static func delete() {
+        // TODO: Delete post
+    }
+}
