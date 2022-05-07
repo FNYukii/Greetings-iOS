8eeef08ee51c08b2108654ab9b6b2c93ab399bb5
Yu357 <forestgreen14689@gmail.com>
2022-05-07 21:31:56 +0900

    feat: ProgressViewを配置

--- Greetings/screens/ProfileView.swift
+++ Greetings/screens/ProfileView.swift
@@ -10,62 +10,84 @@ import SwiftUI
 struct ProfileView: View {
     
     let userId: String
-    @State private var user: User? = nil
     
+    @State private var user: User? = nil
     @State private var posts: [Post] = []
+    
+    @State private var isUserLoaded = false
+    @State private var isPostsLoaded = false
 
     var body: some View {
         
         ScrollView {
-            VStack(alignment: .leading) {
+            VStack {
                 
-                HStack {
-                    Image(systemName: "person.crop.circle")
-                        .font(.largeTitle)
+                if !isUserLoaded {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                }
+                
+                if isUserLoaded {
                     VStack(alignment: .leading) {
-                        Text(user != nil ? user!.displayName : "nothing")
-                            .fontWeight(.bold)
-                        Text(user != nil ? user!.userName : "nothing")
-                            .foregroundColor(.secondary)
+                        HStack {
+                            Image(systemName: "person.crop.circle")
+                                .font(.largeTitle)
+                            VStack(alignment: .leading) {
+                                Text(user != nil ? user!.displayName : "nothing")
+                                    .fontWeight(.bold)
+                                Text(user != nil ? user!.userName : "nothing")
+                                    .foregroundColor(.secondary)
+                            }
+                            Spacer()
+                        }
+                        .padding(.leading)
+                        
+                        Text(user != nil ? user!.introduction : "nothing")
+                            .padding(.horizontal)
+                            .padding(.vertical, 4)
+                        
+                        HStack {
+                            Text(user != nil ? "\(user!.followings.count)" : "nothing")
+                            Text("followings")
+                                .foregroundColor(.secondary)
+                                .padding(.trailing)
+                            Text(user != nil ? "\(user!.followers.count)" : "nothing")
+                            Text("followers")
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(.horizontal)
                     }
-                    Spacer()
                 }
-                .padding(.leading)
-                
-                Text(user != nil ? user!.introduction : "nothing")
-                    .padding(.horizontal)
-                    .padding(.vertical, 4)
-                
-                HStack {
-                    Text(user != nil ? "\(user!.followings.count)" : "nothing")
-                    Text("followings")
-                        .foregroundColor(.secondary)
-                        .padding(.trailing)
-                    Text(user != nil ? "\(user!.followers.count)" : "nothing")
-                    Text("followers")
-                        .foregroundColor(.secondary)
-                }
-                .padding(.horizontal)
                 
                 Divider()
                 
-                ForEach(posts) { post in
-                    PostRow(post: post)
-                        .padding(.horizontal)
-                        .listRowSeparator(.hidden)
+                if !isPostsLoaded {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                }
+                
+                if isPostsLoaded {
+                    ForEach(posts) { post in
+                        PostRow(post: post)
+                            .padding(.horizontal)
+                            .listRowSeparator(.hidden)
+                    }
                 }
             }
         }
-        
         
         .onAppear {
             FireUser.read(id: userId) { user in
                 if let user = user {
                     self.user = user
+                    self.isUserLoaded = true
                 }
             }
             FirePost.read(userId: userId) { posts in
                 self.posts = posts
+                self.isPostsLoaded = true
             }
         }
         
