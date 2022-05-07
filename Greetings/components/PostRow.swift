42530b3a64ebe00462dd16b9dd830ca64ed8f782
Yu357 <forestgreen14689@gmail.com>
2022-05-07 22:10:23 +0900

    feat: 何日前表示を実装

--- Greetings/components/PostRow.swift
+++ Greetings/components/PostRow.swift
@@ -33,7 +33,7 @@ struct PostRow: View {
                             .foregroundColor(.secondary)
                             .lineLimit(1)
                         
-                        Text("12時間前")
+                        HowManyAgoText(from: post.createdAt)
                             .foregroundColor(.secondary)
                         
                         Spacer()
@@ -85,6 +85,37 @@ struct PostRow: View {
             }
         }
         
+    }
+    
+    private func HowManyAgoText(from: Date) -> Text {
+        let inputDate = from
+        
+        let secondDiff: Int = (Calendar.current.dateComponents([.second], from: inputDate, to: Date())).second!
+        if secondDiff < 1 {
+            return Text("now")
+        }
+        if secondDiff < 60 {
+            return Text("\(secondDiff)s")
+        }
+        
+        let minuteDiff = (Calendar.current.dateComponents([.minute], from: inputDate, to: Date())).minute!
+        if minuteDiff < 60 {
+            return Text("\(minuteDiff)m")
+        }
+        
+        let hourDiff = (Calendar.current.dateComponents([.hour], from: inputDate, to: Date())).hour!
+        if hourDiff < 24 {
+            return Text("\(hourDiff)h")
+        }
+        
+        let dayDiff = (Calendar.current.dateComponents([.day], from: inputDate, to: Date())).day!
+        if dayDiff < 31 {
+            return Text("\(dayDiff)d")
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("YYYY MMMM d")
+        return Text(dateFormatter.string(from: from))
     }
 }
 
