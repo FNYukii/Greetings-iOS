9ebf24ffd1e6d446b7015a06ddb061adcc7067ff
Yu357 <forestgreen14689@gmail.com>
2022-05-10 23:32:32 +0900

    feat: 画像加工関連のメソッドはImageEditorクラスにまとめることにした

--- Greetings/utilities/FireImage.swift
+++ Greetings/utilities/FireImage.swift
@@ -5,35 +5,10 @@
 //  Created by Yu on 2022/05/09.
 //
 
-import Foundation
-import UIKit
 import FirebaseStorage
+import SwiftUI
 
 class FireImage {
-    
-    static func toSquare(from: UIImage) -> UIImage {
-        var image = from
-        
-        var oneSide: CGFloat = 0
-        if image.size.width < image.size.height {
-            oneSide = image.size.width
-        } else {
-            oneSide = image.size.height
-        }
-        
-        var origin: CGPoint = CGPoint()
-        if image.size.width < image.size.height {
-            origin = CGPoint(x: 0.0, y: (image.size.width - image.size.height) * 0.5)
-        } else {
-            origin = CGPoint(x: (image.size.height - image.size.width) * 0.5, y: 0.0)
-        }
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: oneSide, height: oneSide), false, 0.0)
-        image.draw(in: CGRect(origin: origin, size: CGSize(width: image.size.width, height: image.size.height)))
-        image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        return image
-    }
     
     static func uploadImage(image: UIImage) {
         let data = image.pngData()!
